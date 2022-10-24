d5cd579a184104b9205d172f107313947b791059
bagidea <thanawat@nexhe.com>
2022-10-24 09:33:34 +0700

    update whitepaper and discord

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -42,6 +42,14 @@ const Header = () => (
                     cursor="pointer"
                 >HOME</Text>
 
+                <Link
+                    href="https://doc.kittymoon.io"
+                >
+                    <Text
+                        cursor="pointer"
+                    >WHITEPAPER</Text>
+                </Link>
+
                 <Text
                     cursor="pointer"
                 >NFT</Text>
@@ -62,9 +70,13 @@ const Header = () => (
                     cursor="pointer"
                 >TEAM</Text>
 
-                <Text
-                    cursor="pointer"
-                >DISCORD</Text>
+                <Link
+                    href="https://discord.gg/rfQBs9KJzx"
+                >
+                    <Text
+                        cursor="pointer"
+                    >DISCORD</Text>
+                </Link>
             </HStack>
         </HStack>
     </Flex>
