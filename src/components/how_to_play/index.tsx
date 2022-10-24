f3015ce7166acf2cb3614c699282baa6e04e86f0
bagidea <thanawat@nexhe.com>
2022-10-24 07:09:47 +0700

    how to play block done.

--- src/components/how_to_play/index.tsx
+++ src/components/how_to_play/index.tsx
@@ -14,6 +14,94 @@ const HowToPlay = () => (
         alignItems="center"
         justifyContent="center"
     >
+        <HStack
+            px="10px"
+            color="white"
+        >
+            <VStack
+                w="450px"
+                padding="20px"
+                border="2px solid"
+                borderColor="rgba(255, 255, 255, 0.2)"
+                spacing="30px"
+                alignItems="start"
+                _hover={ { boxShadow: "rgba(255, 255, 255, 0.2) 0px 0px 30px" } }
+            >
+                <Text
+                    fontSize="28px"
+                    fontWeight="800"
+                >HOW TO PLAY</Text>
+
+                <Flex
+                    w="full"
+                    h="4px"
+                    bgColor="rgba(255, 255, 255, 0.2)"
+                />
+
+                <HStack
+                    fontSize="20px"
+                    fontWeight="600"
+                >
+                    <Flex
+                        w="20px"
+                        h="20px"
+                        bgColor="#4dbc15"
+                        rounded="full"
+                        alignItems="center"
+                        justifyContent="center"
+                    >1</Flex>
+
+                    <Text>EQUIP NFTs</Text>
+                </HStack>
+
+                <Text
+                    fontSize="16px"
+                    fontWeight="600"
+                >Use a hoe for dig an empty space in order to turn it into a space that are suitable for planting.</Text>
+
+                <HStack
+                    fontSize="20px"
+                    fontWeight="600"
+                >
+                    <Flex
+                        w="20px"
+                        h="20px"
+                        bgColor="#4dbc15"
+                        rounded="full"
+                        alignItems="center"
+                        justifyContent="center"
+                    >2</Flex>
+
+                    <Text>PLANTING YOUR SEED AND WATERING</Text>
+                </HStack>
+
+                <Text
+                    fontSize="16px"
+                    fontWeight="600"
+                >You need to use a watering can for watering the seed immediately after the planting is done. Otherwise, the growing period shall not be counted.</Text>
+
+                <HStack
+                    fontSize="20px"
+                    fontWeight="600"
+                >
+                    <Flex
+                        w="20px"
+                        h="20px"
+                        bgColor="#4dbc15"
+                        rounded="full"
+                        alignItems="center"
+                        justifyContent="center"
+                    >3</Flex>
+
+                    <Text>MINE AND COLLECT</Text>
+                </HStack>
+
+                <Text
+                    fontSize="16px"
+                    fontWeight="600"
+                >Use you Axe to harvesting the full-growth plants in order to receive KITTEN Token.</Text>
+            </VStack>
+        </HStack>
     </Flex>
 )
 
