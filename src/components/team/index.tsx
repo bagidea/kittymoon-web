084288c240d69586009aa01c33175af21fe89054
bagidea <thanawat@nexhe.com>
2022-10-27 05:39:07 +0700

    team component done.

--- src/components/team/index.tsx
+++ src/components/team/index.tsx
@@ -10,7 +10,7 @@ const Team = () => (
     <Flex
         position="relative"
         w="full"
-        h="500px"
+        h={ { base: "auto", lg: "500px" } }
         pt="30px"
         bgGradient="linear(to-b, #003010, #4407d1)"
         alignItems="center"
@@ -36,11 +36,12 @@ const Team = () => (
             zIndex="1"
         >
             <Text
-                fontSize="45px"
+                fontSize={ { base: "28px", xl: "45px" } }
                 fontWeight="800"
             >KITTIISM TEAM</Text>
 
             <HStack
+                display={ { base: "none", lg: "flex" } }
                 spacing="100px"
             >
                 <VStack
@@ -118,6 +119,87 @@ const Team = () => (
                     >Art Director</Text>
                 </VStack>
             </HStack>
+
+            <VStack
+                display={ { base: "flex", lg: "none" } }
+                spacing="50px"
+                pb="50px"
+            >
+                <VStack
+                    w="300px"
+                    spacing="0px"
+                >
+                    <Image
+                        src="/ceo.png"
+                        w="200px"
+                    />
+
+                    <Text
+                        fontSize="18px"
+                        fontWeight="600"
+                    >KITTIAUDi</Text>
+
+                    <Text
+                        fontSize="45px"
+                        fontWeight="800"
+                    >CEO</Text>
+
+                    <Text
+                        fontSize="16px"
+                        fontWeight="600"
+                    >Full Stack & Smart Contract Developer</Text>
+                </VStack>
+
+                <VStack
+                    w="300px"
+                    spacing="0px"
+                >
+                    <Image
+                        src="/vp.png"
+                        w="200px"
+                    />
+
+                    <Text
+                        fontSize="18px"
+                        fontWeight="600"
+                    >KITTICUTi</Text>
+
+                    <Text
+                        fontSize="45px"
+                        fontWeight="800"
+                    >VP</Text>
+
+                    <Text
+                        fontSize="16px"
+                        fontWeight="600"
+                    >Game Developer & Legal Director</Text>
+                </VStack>
+
+                <VStack
+                    w="300px"
+                    spacing="0px"
+                >
+                    <Image
+                        src="/avp.png"
+                        w="200px"
+                    />
+
+                    <Text
+                        fontSize="18px"
+                        fontWeight="600"
+                    >KITTIARTi</Text>
+
+                    <Text
+                        fontSize="45px"
+                        fontWeight="800"
+                    >AVP</Text>
+
+                    <Text
+                        fontSize="16px"
+                        fontWeight="600"
+                    >Art Director</Text>
+                </VStack>
+            </VStack>
         </VStack>
     </Flex>
 )
